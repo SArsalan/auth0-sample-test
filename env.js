af63668597c3cadd1612c14d535a491edd8f5596
Arsalan <syed.a.shah@tut.fi>
2017-10-11 11:29:43 +0300

    all files

---
+++ env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  AUTH0_DOMAIN: 'tsm-test.eu.auth0.com',
+  AUTH0_CLIENT_ID: 'wXnWDOYjTlAXD57yG0Puh6j0ZmDVzUNG',
+  AUTH0_AUDIENCE: 'https://tsm-test.eu.auth0.com/userinfo',
+  PACKAGE_ID: 'com.auth0.cordova.example'
+};
